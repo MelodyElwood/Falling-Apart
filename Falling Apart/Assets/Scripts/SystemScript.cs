98f90ec9dcdf80ebc936a2989bd2db3ceeae564f
Ben Elwood <belwood4@gmail.com>
2021-04-13 13:05:07 +1200

    Test

--- Falling Apart/Assets/Scripts/SystemScript.cs
+++ Falling Apart/Assets/Scripts/SystemScript.cs
@@ -7,7 +7,7 @@ public class SystemScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
