e5986bf6a8d9adb2fe2063b4454fd64d52156f85
Ben Elwood <belwood4@gmail.com>
2021-04-16 15:12:23 +1200

    Update SystemScript.cs

--- Falling Apart/Assets/Scripts/SystemScript.cs
+++ Falling Apart/Assets/Scripts/SystemScript.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum SystemType { OXYGEN_GENERATOR, CO2_SCRUBBER, PRESSURIZER, SOLAR_PANELS, BATTERY_CHARGER, MAIN_LIGHTS, BACKUP_LIGHTS, MAIN_COMPUTER };
+
+
 public class SystemScript : MonoBehaviour
 {
+    public SystemType systemType;
+
+    public System system;
     // Start is called before the first frame update
     void Start()
     {
-        //test
+        switch (systemType)
+        {
+            case SystemType.OXYGEN_GENERATOR:
+                system = new Fuse();
+                break;
+            case SystemType.CO2_SCRUBBER:
+                system = new Battery();
+                break;
+            case SystemType.PRESSURIZER:
+                system = new WarningLight();
+                break;
+            case SystemType.SOLAR_PANELS:
+                //system = new WarningBuzzer();
+                break;
+            default:
+                Debug.LogError("Unkown System Type", this);
+                break;
+        }
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public abstract class System
+{
+
 }
