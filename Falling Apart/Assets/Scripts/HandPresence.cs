81260a01573db940ad1ca3acfcb528409b6ad11c
Ben Elwood <belwood4@gmail.com>
2021-06-05 17:02:04 +1200

    Fixes
    - Socket offset
    Potentially:
    - hand presence
    - Teleportation

---
+++ Falling Apart/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public GameObject handPrefab;
+    
+    private InputDevice targetDevice;
+    private GameObject spawnedHand;
+    private Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            UpdateHandAnimation();
+        }
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+
+            spawnedHand = Instantiate(handPrefab, transform);
+            handAnimator = spawnedHand.GetComponent<Animator>();
+        }
+    }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+    }
+}
